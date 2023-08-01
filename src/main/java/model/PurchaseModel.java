f3aef42ff7b86d078cb9c15cbaffb4f157cd1b6f
etylinek <122802525+etylinek@users.noreply.github.com>
2023-08-01 19:17:37 +0200

    AuctionModel and PurchaseModel added

---
+++ src/main/java/model/PurchaseModel.java
@@ -0,0 +1,30 @@
+package model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class PurchaseModel {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+//    @OneToOne
+//    private AuctionModel id;
+
+    @Column(name = "value")
+    private BigDecimal value;
+}
---
+++ src/main/java/repository/PurchaseRepository.java
@@ -0,0 +1,10 @@
+package repository;
+
+
+import model.PurchaseModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PurchaseRepository extends JpaRepository<PurchaseModel,Long> {
+}
