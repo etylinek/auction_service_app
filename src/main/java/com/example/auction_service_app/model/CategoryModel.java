40d7ffac052d3e26c47cada5242a89bdb0a8f1bb
Bezbolesny <krzysztof.t911@gmail.com>
2023-08-01 19:02:43 +0200

    Added: 1) model package: - CategoryModel - PurchaseRatingModel 2) repository package: - CategoryRepository - PurchaseRatingRepository

---
+++ src/main/java/com/example/auction_service_app/model/CategoryModel.java
@@ -0,0 +1,29 @@
+package com.example.auction_service_app.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+public class CategoryModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+//    @Column(name = "logoImage")
+//    private String logoImage;
+
+}
