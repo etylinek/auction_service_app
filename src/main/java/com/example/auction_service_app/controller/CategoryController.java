9858215db3793b27280057a6b9d22b3aaff8ff04
Bezbolesny <krzysztof.t911@gmail.com>
2023-08-02 19:56:02 +0200

    Changed getCategoryList method in CategoryController class and updated CategoryService class.

--- src/main/java/com/example/auction_service_app/controller/CategoryController.java
+++ src/main/java/com/example/auction_service_app/controller/CategoryController.java
@@ -2,8 +2,10 @@ package com.example.auction_service_app.controller;
 
 import com.example.auction_service_app.model.CategoryModel;
 import com.example.auction_service_app.service.CategoryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/categories")
 public class CategoryController {
 
-    @Autowired
-    private CategoryService categoryService;
+    private final CategoryService categoryService;
 
     @GetMapping
-    public List<CategoryModel> getAllCategories() {
-        return categoryService.getAllCategories();
+    public String getCategoryList(Model model){
+        List<CategoryModel> categoryList = categoryService.getAllCategories();
+        model.addAttribute("categoryModel", categoryList);
+        return "/categoryList"; //widok do dodania
     }
 }
