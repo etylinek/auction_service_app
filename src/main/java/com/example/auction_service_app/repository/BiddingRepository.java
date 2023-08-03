6679a7529629d94b175e0ff406668beb697683df
tomas <tomaszkozak1996@gmail.com>
2023-08-03 20:02:57 +0200

    Feature/ASA-1.5&&1.6_
    Added: prezentacja listy aukcji, które licytuje zalogowany user (z punktu 1.5)
    Added: prezentacja listy aukcji obserwowanych (z punktu 1.6)

--- src/main/java/com/example/auction_service_app/repository/BiddingRepository.java
+++ src/main/java/com/example/auction_service_app/repository/BiddingRepository.java
@@ -1,7 +1,12 @@
 package com.example.auction_service_app.repository;
 
+import com.example.auction_service_app.model.AuctionModel;
 import com.example.auction_service_app.model.BiddingModel;
+import com.example.auction_service_app.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BiddingRepository extends JpaRepository<BiddingModel, Long> {
+    List<BiddingModel> findByUserModel(UserModel user);
 }
