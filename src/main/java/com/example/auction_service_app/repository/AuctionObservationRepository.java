23689c92071d3a48545760c8e69e9515c93d412b
Bezbolesny <krzysztof.t911@gmail.com>
2023-08-03 19:04:00 +0200

    Changed classes names - AuctionObservationModel and BiddingModel

--- src/main/java/com/example/auction_service_app/repository/AuctionObservationRepository.java
+++ src/main/java/com/example/auction_service_app/repository/AuctionObservationRepository.java
@@ -1,9 +1,9 @@
 package com.example.auction_service_app.repository;
 
-import com.example.auction_service_app.model.AuctionObservation;
+import com.example.auction_service_app.model.AuctionObservationModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AuctionObservationRepository extends JpaRepository<AuctionObservation, Long> {
+public interface AuctionObservationRepository extends JpaRepository<AuctionObservationModel, Long> {
 }
--- src/main/java/com/example/auction_service_app/repository/BiddingRepository.java
+++ src/main/java/com/example/auction_service_app/repository/BiddingRepository.java
@@ -1,7 +1,7 @@
 package com.example.auction_service_app.repository;
 
-import com.example.auction_service_app.model.Bidding;
+import com.example.auction_service_app.model.BiddingModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BiddingRepository extends JpaRepository<Bidding, Long> {
+public interface BiddingRepository extends JpaRepository<BiddingModel, Long> {
 }
