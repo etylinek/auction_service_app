e4055ba2d40db54001f8e68ad5095b1a67bb8f75
Bezbolesny <krzysztof.t911@gmail.com>
2023-08-03 20:07:33 +0200

    Some changes to fix adding new user

--- src/main/java/com/example/auction_service_app/controller/UserController.java
+++ src/main/java/com/example/auction_service_app/controller/UserController.java
@@ -1,24 +1,19 @@
 package com.example.auction_service_app.controller;
 
-import com.example.auction_service_app.model.AuctionModel;
-import com.example.auction_service_app.model.AuctionObservation;
-import com.example.auction_service_app.model.Bidding;
 import com.example.auction_service_app.model.UserModel;
 import com.example.auction_service_app.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
-import java.security.Principal;
-import java.util.List;
-
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
     @GetMapping("/addUser")
     public String getAddUser(){
