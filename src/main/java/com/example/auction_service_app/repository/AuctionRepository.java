759e7526071e5a5283ff9836b6e8992fbf0015e3
tomas <tomaszkozak1996@gmail.com>
2023-08-10 18:12:11 +0200

    Fix/ASA_5.1&5.2_Search_3 Search method now will work for upper and lower case AS WELL AS parts of String (keyword).

--- src/main/java/com/example/auction_service_app/repository/AuctionRepository.java
+++ src/main/java/com/example/auction_service_app/repository/AuctionRepository.java
@@ -16,8 +16,8 @@ public interface AuctionRepository extends JpaRepository<AuctionModel,Long> {
     List<AuctionModel> findByUserModel(UserModel user); // Potrzebne do punktu 1.4 prezentacja listy aukcji usera (alternatywnie może być zalogowanego)
 
     // Uproszczony kod do wyszukiwania aukcji na podstawie słowa kluczowego.
-    @Query("SELECT a FROM AuctionModel a WHERE LOWER(a.name) = LOWER(:name)")
-    List<AuctionModel> findByNameIgnoreCase(@Param("name") String name);
+    @Query("SELECT a FROM AuctionModel a WHERE LOWER(a.name) LIKE LOWER(CONCAT('%', :name, '%'))")
+    List<AuctionModel> findByNameContainingIgnoreCase(String name);
 
     // Uproszczony kod do wyszukiwania aukcji na podstawie kategorii.
     List<AuctionModel> findByCategoryModel(CategoryModel categoryModel);
